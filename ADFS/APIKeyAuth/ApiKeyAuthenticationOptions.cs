2da1c96c14fdd1174e6810ae2a18ce93ada3c2f6
dk <dk@nwth.rui>
2021-04-19 19:43:28 +0300

    Добавлен обработчик APIKey

---
+++ ADFS/APIKeyAuth/ApiKeyAuthenticationOptions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADFS.APIKeyAuth
+{
+    public class ApiKeyAuthenticationOptions : AuthenticationSchemeOptions
+    {
+        public const string DefaultScheme = "APIKey";
+        public string Scheme => DefaultScheme;
+        public string AuthenticationType = DefaultScheme;
+    }
+}
---
+++ ADFS/APIKeyAuth/IGetApiKeyQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADFS.APIKeyAuth
+{
+    public interface IGetApiKeyQuery
+    {
+        Task<ApiKey> Execute(string providedApiKey);
+    }
+}
